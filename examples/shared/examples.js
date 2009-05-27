980062e578460337ea26e99e7b599b52f087c54a
Meir Kriheli <meir@mksoft.co.il>
2009-05-27 17:08:31 +0300

    Revert Ext.BLANK_IMAGE_URL to default

--- examples/shared/examples.js
+++ examples/shared/examples.js
@@ -2,11 +2,11 @@
  * Ext JS Library 3.0 Pre-alpha
  * Copyright(c) 2006-2008, Ext JS, LLC.
  * licensing@extjs.com
- * 
+ *
  * http://extjs.com/license
  */
 
-Ext.BLANK_IMAGE_URL = '../../resources/images/default/s.gif';
+//Ext.BLANK_IMAGE_URL = '../../resources/images/default/s.gif';
 
 Ext.example = function(){
     var msgCt;
