0dcd6a373951578ee08e6cf8510f0a2fad455950
Meir Kriheli <mkriheli@gmail.com>
2011-11-19 13:57:27 +0200

    Make sure onLayout works for newer 3.x  branches

--- src/extjs_rtl.js
+++ src/extjs_rtl.js
@@ -22,7 +22,8 @@ Ext.override(Ext.layout.HBoxLayout,{
         Ext.layout.HBoxLayout.superclass.onLayout.call(this, ct, target);
 
         var cs = ct.items.items, len = cs.length, c, i, last = len-1, cm;
-        var size = this.getLayoutTargetSize(target);
+
+        var size = typeof(this.getLayoutTargetSize) === 'undefined' ? this.getTargetSize(target) : this.getLayoutTargetSize(target);
 
         var w = size.width - target.getPadding('lr') - this.scrollOffset,
             h = size.height - target.getPadding('tb'),
@@ -120,7 +121,7 @@ Ext.override(Ext.layout.VBoxLayout, {
         Ext.layout.VBoxLayout.superclass.onLayout.call(this, ct, target);
 
         var cs = ct.items.items, len = cs.length, c, i, last = len-1, cm;
-        var size = this..getLayoutTargetSize(target);
+        var size = typeof(this.getLayoutTargetSize) === 'undefined' ? this.getTargetSize(target) : this.getLayoutTargetSize(target);
 
         var w = size.width - target.getPadding('lr') - this.scrollOffset,
             h = size.height - target.getPadding('tb'),
